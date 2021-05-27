b327b3981c20b56fa734dd933bcf13d8dd57ef1e
Alessandro Marinuzzi <alessandromarinuzzi@gmail.com>
2021-05-27 13:38:20 +0100

    - Generate a single random emailAddress

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,19 @@
 const root = new Vue({
     el:"#root",
 
-    data:"",
+    data:{
+        emailAddress:"",
+    },
 
     methods:"",
 
     mounted(){
-        
+        axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then(response =>{
+            // console.log(response.data.response);
+            emailAddress = response.data.response
+            console.log(emailAddress);
+        })
     }
 })
